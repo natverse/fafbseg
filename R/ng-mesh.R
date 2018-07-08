9d506127049f0f42031908a6c727b5146af66d38
Gregory Jefferis <jefferis@gmail.com>
2018-07-08 13:00:39 +0100

    Initial support for 3d mesh functions
    * xyzmatrix provides a bridge to nat

---
+++ R/ng-mesh.R
@@ -0,0 +1,55 @@
+#' Extract the XYZ coordinates from neuroglancer mesh objects
+#'
+#' @export
+#' @importFrom nat xyzmatrix
+#' @rdname xyzmatrix
+#' @aliases xyzmatrix
+#' @seealso \code{\link[nat]{xyzmatrix}}
+xyzmatrix.ng_raw <- function(x, ...) {
+  x$coords
+}
+
+#' @export
+#' @importFrom nat xyzmatrix
+#' @rdname xyzmatrix
+xyzmatrix.ng_raw_list <- function(x, ...) {
+  emptymat=matrix(nrow=0, ncol=3, dimnames = list(NULL, c("X","Y","Z")))
+  if(!length(x)) return(emptymat)
+  do.call(rbind, lapply(x, xyzmatrix))
+}
+
+#' @importFrom rgl as.mesh3d
+#' @name as.mesh3d
+#' @export
+NULL
+
+#' Convert an raw neuroglancer mesh into a mesh3d object
+#'
+#' @param x An ng_raw or ng_raw_list object
+#' @param ... additional arguments passed to \code{\link{tmesh3d}}
+#' @rdname as.mesh3d
+#' @export
+as.mesh3d.ng_raw <- function(x, ...) {
+  rgl::tmesh3d(vertices = t(x$coords), indices = t(x$indices)+1, homogeneous = F, ...)
+}
+
+#' @export
+#' @rdname as.mesh3d
+as.mesh3d.ng_raw_list <- function(x, ...) {
+  y=unlist(x)
+  as.mesh3d(y)
+}
+
+#' @export
+unlist.ng_raw_list <- function(x, ...){
+  rawindexlist=lapply(x, function(y) y$indices)
+
+  nrows=sapply(x, function(y) nrow(y$coords))
+  # we need to make an offset to the indices
+  offsets=c(0,cumsum(nrows[-length(nrows)]))
+  corindices=mapply('+', rawindexlist, offsets, SIMPLIFY = F)
+  res=list(coords=xyzmatrix(x),
+           indices=do.call(rbind, corindices))
+  class(res)='ng_raw'
+  res
+}
