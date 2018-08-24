e97978d381ebd23d5a56e97f97914de8f315d3b5
Gregory Jefferis <jefferis@gmail.com>
2018-08-24 11:45:41 +0100

    Support for reading skeletons from zip files

---
+++ R/skeleton-io.R
@@ -0,0 +1,61 @@
+#' Read skeletons for segments by extracting from the corresponding zip file(s)
+#'
+#' @param x A vector of segment ids (you might get these from a neuroglancer
+#'   scene)
+#' @param voxdims The voxel dimensions in nm of the skeletonised data
+#' @param ... additional arguments passed to \code{\link[nat]{read.neurons}}
+#' @return A \code{\link[nat]{neuronlist}} containing one
+#'   \code{\link[nat]{neuron}} for each fragment
+#' @seealso \code{\link[nat]{read.neurons}}
+#' @importFrom nat read.neurons
+#' @export
+#' @examples
+#' \donttest{
+#' n <- read_segments(7574605868)
+#' }
+read_segments <- function(x, voxdims=c(32,32,40), ...) {
+  # fl will be a list
+  ff=character()
+  for(seg in x) {
+    skels=skelsforsegment(x)
+    ff=c(ff, extract_zip_files(skels))
+  }
+  # If we keep on memoising zip lists, we'll quickly use a lot of memory ...
+  memoise::forget(zip_list_m)
+  # it will be useful to know the segment / fragment ids
+  df=as.data.frame(swc2segmentid(ff, include.fragment = TRUE))
+  rownames(df)=basename(ff)
+  suppressMessages(suppressWarnings(read.neurons(ff, df = df, ...)*voxdims))
+}
+
+#' Find all skeleton fragments for one segment
+#'
+#' @param x A segment id
+#' @return a character vector of fragment names with an attribute containing the
+#'   path to the zip file
+skelsforsegment <- function(x) {
+  zip=segmentid2zip(x)
+  zipp=zip_path(zip)
+  zl=zip_list_m(zipp)
+  matches=grep(paste0("^", x,"\\."), zl[['filename']], useBytes = T, value = T, perl=T)
+  attr(matches,'zip')=zipp
+  matches
+}
+
+#' @importFrom nat read.neurons
+read_topn <- function(zipfile, n=1, exdir=tempfile(), ...) {
+  if(!file.exists(exdir)) {
+    dir.create(exdir)
+    on.exit(unlink(exdir, recursive = T))
+  }
+  pp=character()
+  for(z in zipfile) {
+    zl=zip::zip_list(z)
+    idx=order(zl$uncompressed_size, decreasing = TRUE)
+    selidx=idx[seq_len(n)]
+    p=unzip(z, files = zl$filename[selidx], exdir = exdir, junkpaths=T)
+    # FIXME, but probably tiny
+    pp=c(pp, p)
+  }
+  read.neurons(pp, ...)*c(32,32,40)
+}
---
+++ R/zip.R
@@ -0,0 +1,70 @@
+#' @importFrom utils unzip
+#' @importFrom progress progress_bar
+extract_zip_files <- function(files, zipfiles=NULL, ...) {
+  if(is.character(files)) {
+    zipfiles=attr(files, 'zip')
+    files=list(files)
+  } else {
+    if(is.null(zipfiles)) zipfiles=names(zipfiles)
+  }
+
+  if(is.null(zipfiles)) {
+    stop("Please specify the zipfiles containing the files to extract",
+         " as described in ?extract_zip_files")
+  }
+  names(files)=zipfiles
+
+  nzips=length(zipfiles)
+  if(nzips>1 && length(files)!=nzips) {
+    stop("files must be a list with one character vector for each zip file!")
+  }
+
+  nfiles=length(unlist(files))
+  pb <- progress_bar$new(total = nfiles, show_after=0.5,
+    format = "  extracting [:bar] :current/:total eta: :eta")
+
+  ff=character()
+  for(zipfile in zipfiles) {
+    exdir=tempdir4zip(zipfile)
+    x=files[[zipfile]]
+    pb$tick(length(x))
+    ff=c(ff, unzip(zipfile, x, exdir = exdir, junkpaths=T, overwrite = F, ...))
+  }
+  ff
+}
+
+# memoised so we only run once
+#' @importFrom memoise memoise is.memoised
+temproot <- function() {
+  td <- tempfile('fafbseg-zips')
+  dir.create(td)
+  td
+}
+if(!memoise::is.memoised(temproot)) memoise::memoise(temproot)
+
+tempdir4zip <- function(x) {
+  zipstem=tools::file_path_sans_ext(basename(x))
+  fp=file.path(temproot(), zipstem)
+  dir.create(fp, recursive = T)
+  fp
+}
+if(!memoise::is.memoised(tempdir4zip)) memoise::memoise(tempdir4zip)
+
+zip_path <- function(x, root=getOption('fafbseg.skelziproot', NULL), mustWork=NA) {
+  if(is.null(root))
+    stop("Please set options(fafbseg.skelziproot='path/to/zips') to set location of skeleton zip files!")
+  p=file.path(root, x)
+  if(isTRUE(!mustWork))
+    return(p)
+  up=unique(p)
+  FUN=ifelse(isTRUE(mustWork), stop, warning)
+  fexists <- file.exists(up)
+  if(!all(fexists))
+    FUN("Cannot find zips: ", paste(basename(up[!fexists]), collapse = " "),
+        " at path: ", root)
+  p
+}
+
+#' @importFrom memoise memoise
+#' @importFrom zip zip_list
+zip_list_m <- memoise::memoise(zip::zip_list)
