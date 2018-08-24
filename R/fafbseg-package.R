3fcd70c1fba0eb801f76a9eb8e76d3e1d407aba3
Gregory Jefferis <jefferis@gmail.com>
2018-08-24 12:51:07 +0100

    package docs

--- R/fafbseg-package.R
+++ R/fafbseg-package.R
@@ -1,2 +1,16 @@
 #' @keywords internal
+#' @section Package Options: \itemize{
+#'
+#'   \item{\code{fafbseg.skelziproot}} set to the location of a folder
+#'   containing the zipped versions of the skeletonised segmentations. This will
+#'   be used by \code{\link{read_segments}}, \code{\link{read_topn}} etc.
+#'
+#'   \item{\code{fafbseg.sampleurl}} optionally set to a sample Neuroglancer URL
+#'   that will modified to point to arbitrary locations by
+#'   \code{\link{open_fafb_ngl}}.
+#'
+#'   }
+#'
+#' @examples
+#' options()[grep("^fafbseg\\.", names(options()))]
 "_PACKAGE"
