fcd2c538e577aaea47486b45c754c889e9d98adc
Gregory Jefferis <jefferis@gmail.com>
2018-08-24 17:56:17 +0100

    missing pipe import

---
+++ R/utils-pipe.R
@@ -0,0 +1,11 @@
+# #' Pipe operator
+# #'
+# #' See \code{magrittr::\link[magrittr]{\%>\%}} for details.
+# #'
+# #' @rdname pipe
+# #' @keywords internal
+# #' @export
+# #' @usage lhs \%>\% rhs
+# #' @name %>%
+#' @importFrom magrittr %>%
+NULL
