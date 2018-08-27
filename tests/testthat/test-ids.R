c942f7fcc253a337c882c1bcc3f0093d5e7e0189
Gregory Jefferis <jefferis@gmail.com>
2018-08-27 19:48:37 +0100

    add ngl_segments
    * withs tests and docs

--- tests/testthat/test-ids.R
+++ tests/testthat/test-ids.R
@@ -14,3 +14,20 @@ test_that("simple ids", {
 
   expect_equal(segmentid2zip(swc2segmentid("10001654273.1.swc")), "100016.zip")
 })
+
+
+test_that('ngl_segments', {
+  baseline=c(10950626347, 10952282491, 13307888342)
+  # json file
+  expect_equal(ngl_segments("testdata/testscene.json"), baseline)
+  # json text
+  expect_equal(ngl_segments(readLines("testdata/testscene.json")), baseline)
+  # R list
+  scene=jsonlite::fromJSON("testdata/testscene.json")
+  expect_equal(ngl_segments(scene), baseline)
+  expect_equal(ngl_segments(ngl_encode_url(body = scene)), baseline)
+
+  # list with two sets of segments
+  expect_error(ngl_segments("testdata/testscene-double.json"))
+})
+
