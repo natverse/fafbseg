4d82587fd09057aa606d4db2a11002a69204f136
Gregory Jefferis <jefferis@gmail.com>
2018-10-10 18:07:44 +0100

    Teach find_merged_segments to return merge groups
    * optional

--- R/merge-groups.R
+++ R/merge-groups.R
@@ -2,6 +2,8 @@
 #'
 #' @param x a segment id or any other input that can be interpreted by
 #'   \code{\link{ngl_segments}}
+#' @param return.groups Whether to return the merge groups as well as the
+#'   segment ids
 #' @return vector of segment ids (in ascending order)
 #' @details segment ids in \code{ffn16reseg-ms1000_md0.02_c0.6_iou0.7} always
 #'   match one raw segment id in \code{fafb_v14_16nm_v00c_split3xfill2} but may
@@ -15,8 +17,10 @@
 #' @examples
 #' \donttest{
 #' find_merged_segments(7186840767)
+#' # these all belong to one merge group by definition
+#' find_merged_segments(7186840767, return.groups=TRUE)
 #' }
-find_merged_segments <- function(x) {
+find_merged_segments <- function(x, return.groups=FALSE) {
   if(!requireNamespace('fafbsegdata', quietly = TRUE)) {
     stop('Please do:\n',
          'devtools::install_github("jefferis/fafbsegdata")\n',
@@ -24,11 +28,17 @@ find_merged_segments <- function(x) {
   }
   x=ngl_segments(x)
   groups=fafbsegdata::segment_merge_groups.dt[id%in%x,][['membership']]
-  groups=unique(groups)
-  if(length(groups)) {
+  ugroups=unique(groups)
+  if(length(ugroups)) {
     # some segments had merge information
-    merge_group_ids=fafbsegdata::segment_merge_groups.dt[membership%in%groups,][['id']]
+    merge_group_df=fafbsegdata::segment_merge_groups.dt[membership%in%ugroups,]
+    merge_group_ids=merge_group_df[['id']]
     x <- union(x, merge_group_ids)
   }
-  sort(x)
+  x=sort(x)
+  if(return.groups) {
+    m=match(x, merge_group_df[['id']])
+    groups=merge_group_df[['membership']][m]
+    data.frame(segment=x, group=groups)
+  } else x
 }
