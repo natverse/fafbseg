81629cfbb9fbfc604f69f189f3812211c31919c3
Gregory Jefferis <jefferis@gmail.com>
2018-10-29 22:33:51 +0000

    correct URI for latest (post-agglom) skeletons
    * _b was missing from gs:// path so got confused

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
     fafbseg.sampleurl = "https://neuroglancer-demo.appspot.com/#!%7B%22layers%22:%7B%22fafb_v14_clahe%22:%7B%22source%22:%22brainmaps://772153499790:fafb_v14:fafb_v14_clahe%22,%22type%22:%22image%22%7D,%22fafb_v14_16nm_v00c_split3xfill2%22:%7B%22source%22:%22brainmaps://772153499790:fafb_v14:fafb_v14_16nm_v00c_split3xfill2%22,%22type%22:%22segmentation%22%7D%7D,%22navigation%22:%7B%22pose%22:%7B%22position%22:%7B%22voxelSize%22:[8,8,40],%22voxelCoordinates%22:[53323.1017,21251.8736,3282]%7D%7D,%22zoomFactor%22:8%7D,%22showAxisLines%22:false,%22perspectiveOrientation%22:[0.0214,0.4873,-0.505,-0.7121],%22perspectiveZoom%22:1024,%22showSlices%22:false%7D",
     fafbseg.brainmaps.volume="772153499790:fafb_v14:fafb_v14_16nm_v00c_split3xfill2",
     fafbseg.brainmaps.meshName="mcws_quad1e6",
-    fafbseg.skeletonuri="brainmaps://772153499790:fafb_v14:fafb_v14_16nm_v00c_split3xfill2_flatreseg2_skeletons32/teasar512_nnconn165_mc10000_prune10_thresh1000_sparse250"
+    fafbseg.skeletonuri="brainmaps://772153499790:fafb_v14:fafb_v14_16nm_v00c_split3xfill2_flatreseg2_skeletons32/teasar512_nnconn165_mc10000_prune10_thresh1000_sparse250_b"
 
   )
 
