dd1903d9c4d6532cd67fc433801a659a69de5a1d
Gregory Jefferis <jefferis@gmail.com>
2019-04-11 09:36:06 +0100

    Help for catmaid2ngl

--- R/catmaid-ngl.R
+++ R/catmaid-ngl.R
@@ -1,8 +1,14 @@
-# Convert CATMAID things (eg neurons) into Neuroglancer equivalents
+#' Convert CATMAID things (eg neurons) into Neuroglancer equivalents
 #' @export
+#' @param x An object to convert (see Descriptions for each method)
+#' @param ... Additional arguments passed to methods
 catmaid2ngl <- function(x, ...) UseMethod('catmaid2ngl')
 
 #' @export
+#' @rdname catmaid2ngl
+#' @description \code{catmaid2ngl.neuron} uses \code{\link{brainmaps_xyz2id}}
+#'   and \code{\link{read_segments2}} to find the skeletons corresponding to a
+#'   (catmaid) neuron
 catmaid2ngl.neuron <- function(x, ...) {
   # readfun=c("read_segments2", "read.neuron.brainmaps")
   df = as.data.frame(xyzmatrix(x))
@@ -21,18 +27,30 @@ catmaid2ngl.neuron <- function(x, ...) {
 }
 
 #' @export
+#' @rdname catmaid2ngl
 #' @importFrom nat nlapply
+#' @inheritParams nat::nlapply
+#' @description \code{catmaid2ngl.neuronlist} applies \code{catmaid2ngl.neuron}
+#'   over a \code{\link{neuronlist}}
 catmaid2ngl.neuronlist <- function(x, OmitFailures=TRUE, ...) {
   nlapply(x, catmaid2ngl, OmitFailures=OmitFailures, ...)
 }
 
+#' @rdname catmaid2ngl
 #' @export
+#' @inheritParams open_fafb_ngl
+#' @description \code{catmaid2ngl.character} uses \code{\link{open_fafb_ngl}} to
+#'   open CATMAID at a location matching a braingazer URL.
 catmaid2ngl.character <- function(x, open=FALSE, ...) {
   open_fafb_ngl(x, open=open, ...)
 }
 
+#' @rdname catmaid2ngl
 #' @export
 #' @importFrom catmaid read.neuron.catmaid catmaid_skids
+#' @description Converts a CATMAID skid specification (see
+#'   \code{\link{catmaid_skids}}) for one neuron into a an autosegmentation
+#'   based neuron using \code{catmaid2ngl.neuron}.
 catmaid2ngl.default <- function(x, ...) {
   x=read.neuron.catmaid(catmaid_skids(x, several.ok=FALSE, ...), ...)
   catmaid2ngl(x)
