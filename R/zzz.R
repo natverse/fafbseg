d553b58519fb7b91dbdb6812e30ab0a4ec9fd734
Gregory Jefferis <jefferis@gmail.com>
2019-08-17 18:02:13 +0100

    simplify .onLoad by using choose_segmentation

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  op.fafbseg=list(
-    fafbseg.sampleurl = "https://neuroglancer-demo.appspot.com/#!%7B%22layers%22:%7B%22fafb_v14_clahe%22:%7B%22source%22:%22brainmaps://772153499790:fafb_v14:fafb_v14_clahe%22,%22type%22:%22image%22%7D,%22fafb_v14_16nm_v00c_split3xfill2%22:%7B%22source%22:%22brainmaps://772153499790:fafb_v14:fafb_v14_16nm_v00c_split3xfill2%22,%22type%22:%22segmentation%22%7D%7D,%22navigation%22:%7B%22pose%22:%7B%22position%22:%7B%22voxelSize%22:[8,8,40],%22voxelCoordinates%22:[53323.1017,21251.8736,3282]%7D%7D,%22zoomFactor%22:8%7D,%22showAxisLines%22:false,%22perspectiveOrientation%22:[0.0214,0.4873,-0.505,-0.7121],%22perspectiveZoom%22:1024,%22showSlices%22:false%7D",
-    fafbseg.brainmaps.volume="772153499790:fafb_v14:fafb_v14_16nm_v00c_split3xfill2",
-    fafbseg.brainmaps.meshName="mcws_quad1e6",
-    fafbseg.skeletonuri="brainmaps://772153499790:fafb_v14:fafb_v14_16nm_v00c_split3xfill2_flatreseg2_skeletons32/teasar512_nnconn165_mc10000_prune10_thresh1000_sparse250_b"
-
-  )
+  op.fafbseg=choose_segmentation('20190805', set=FALSE)
 
   op<-options()
   toset <- !(names(op.fafbseg) %in% names(op))
@@ -44,8 +38,8 @@ find_zip_divisor <- function(zipdir) {
 
 # in future may use memoised version of this to avoid explictly
 # setting the fafbseg.zipdivisor option
-# althoug querying an option is very fast - even compared with getting the
-# results of a memoised function
+# although querying an option is very fast - even compared with getting the
+# results of a memoised function (~)
 # ofind_zip_divisor <- memoise::memoise(ofind_zip_divisor)
 
 
