aab4b9493533ff0847202224da0778edeb760df1
Gregory Jefferis <jefferis@gmail.com>
2020-11-02 08:06:53 +0000

    Add python+cloudvolume+skeletor * uses bionic + deadsnakes python 3.7 * strangely I see 3.6 on vfbconnectr with this config   (see https://travis-ci.com/github/jefferis/vfbconnectr/builds/199171545) * but otherwise OK

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -4,8 +4,9 @@ test_that("dr_fafbseg works", {
 })
 
 test_that("pyids2bit64 works", {
-  skip_if_not_installed('reticulate')
   skip_if_not_installed('bit64')
+  skip_if_not_installed('reticulate')
+  skip_if_not(reticulate::py_available())
   reticulate::py_run_string('import numpy as np')
 
   sids=c("720575940625861628","720575940621611957")
