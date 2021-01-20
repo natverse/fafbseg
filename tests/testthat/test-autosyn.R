773de5887339b99b45ba2ff620b5679822edc822
Gregory Jefferis <jefferis@gmail.com>
2021-01-20 03:32:49 +0000

    add test for flywire_partners both

--- tests/testthat/test-autosyn.R
+++ tests/testthat/test-autosyn.R
@@ -1,4 +1,4 @@
-test_that("flywire_partner_summary works", {
+test_that("flywire_partners / flywire_partner_summary works", {
   token=try(chunkedgraph_token(), silent = TRUE)
   skip_if_not_installed('reticulate')
   skip_if(inherits(token, "try-error"),
@@ -6,12 +6,23 @@ test_that("flywire_partner_summary works", {
   skip_if_not(reticulate::py_module_available("cloudvolume"),
               "Skipping live flywire tests requiring python cloudvolume module")
 
-  expect_message(df <- flywire_partner_summary("720575940615237849", Verbose = T),
-                'Fetching supervoxel.*720575940615237849')
+  expect_message(df <- flywire_partner_summary("720575940616243077", Verbose = T),
+                'Fetching supervoxel.*720575940616243077')
   expect_is(df, 'data.frame')
   expect_named(df, c("post_id", "weight", "n"))
-  expect_message(df2 <- flywire_partner_summary("720575940615237849", partners = 'input', Verbose = T),
-                 'Fetching supervoxel.*720575940615237849')
+  expect_message(df2 <- flywire_partner_summary("720575940616243077", partners = 'input', Verbose = T),
+                 'Fetching supervoxel.*720575940616243077')
+
+  expect_is(ins <- flywire_partners("720575940616243077", partners = 'inputs'), 'data.frame')
+  expect_is(outs <- flywire_partners("720575940616243077", partners = 'outputs'), 'data.frame')
+
+  expect_equal(nrow(ins), 156L)
+  expect_equal(nrow(outs), 463L)
+  both=flywire_partners("720575940616243077", partners = 'both')
+  expect_true(all(ins$offset %in% both$offset))
+  expect_true(all(outs$offset %in% both$offset))
+  expect_true(all(both$offset %in% c(outs$offset, ins$offset)))
+
   skip_if_not_installed('bit64')
   kcs=bit64::as.integer64(c("720575940609992371","720575940623755722"))
   expect_is(flywire_partners(kcs), 'data.frame')
