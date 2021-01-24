b7c651298b9ba94ef27af42a810ad67e8d17a7ed
Gregory Jefferis <jefferis@gmail.com>
2021-01-24 19:19:52 +0000

    furthee dox tweak

--- R/autosyn.R
+++ R/autosyn.R
@@ -486,8 +486,9 @@ flywire_ntplot3d <- function(x, nts=c("gaba", "acetylcholine", "glutamate",
 #' @param remove_autapses  whether to remove autapses (defaults to \code{TRUE}).
 #' @param transmitters if \code{TRUE} also attempt to retrieve neurotransmitter
 #'   predictions from Eckstein et al. 2020, for the flywire neuron in question.
-#' @param cleft.threshold when >0 apply select only synaptic connections
-#'   exceeding this confidence threshold.
+#' @param cleft.threshold select only synaptic connections exceeding this
+#'   confidence threshold (default of 0 uses all synapses; values in the range
+#'   30-100 seem to make sense).
 #' @param ... methods sent to \code{nat::nlapply}.
 #' @inheritParams flywire_partners
 #' @inheritParams flywire_ntpred
