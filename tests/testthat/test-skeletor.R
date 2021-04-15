ed75d8091dc4ada3b18270655d51d331a10e5e6c
alexanderbates <alexander.shakeel.bates@gmail.com>
2021-04-14 21:43:19 -0400

    Change the skeletor test

--- tests/testthat/test-skeletor.R
+++ tests/testthat/test-skeletor.R
@@ -6,8 +6,8 @@ test_that("skeletor works", {
               "Skipping live flywire tests requiring python skeletor module")
   cv_available=!inherits(try(check_cloudvolume_reticulate(), silent = TRUE), "try-error")
   skip_if_not(cv_available, 'skipping tests requiring cloudvolume')
-  xyz <- structure(c(375741.718773652, 375624.31567637, 175945.443753022,
-                     176156.125824337, 211280, 211160), .Dim = 2:3, .Dimnames = list(
+  xyz <- structure(c(713027.626386585, 713042.051094728, 133955.812509071,
+                     134126.394370907, 123640, 123480), .Dim = 2:3, .Dimnames = list(
                        NULL, c("X", "Y", "Z")))
   ids <- unique(flywire_xyz2id(xyz))
   neuron <- skeletor(ids, heal = FALSE, method = "wavefront")
