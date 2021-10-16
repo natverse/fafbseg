b6b65b04d3a41043975af2833f95a8db521cfe73
Gregory Jefferis <jefferis@gmail.com>
2021-10-16 14:38:15 +0100

    add test-flytable

---
+++ tests/testthat/test-flytable.R
@@ -0,0 +1,10 @@
+test_that("query works", {
+
+  ac=try(flytable_login())
+  skip_if(inherits(ac, 'try-error'),
+          "skipping flytable tests as uable to login!")
+
+  expect_s3_class(df <- flytable_query("hemilineages", "select FLYWIREsvid, hemibrain_match FROM fafb_hemilineages_survey WHERE hemibrain_match!='' limit 5"),
+                  'data.frame')
+  expect_equal(nrow(df), 5L)
+})
