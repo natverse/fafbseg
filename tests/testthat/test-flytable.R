19336837281676a9cdaef61ec6cae16a5c6a1f5e
Gregory Jefferis <jefferis@gmail.com>
2021-10-17 15:56:00 +0100

    typo

--- tests/testthat/test-flytable.R
+++ tests/testthat/test-flytable.R
@@ -2,7 +2,7 @@ test_that("query works", {
 
   ac=try(flytable_login())
   skip_if(inherits(ac, 'try-error'),
-          "skipping flytable tests as uable to login!")
+          "skipping flytable tests as unable to login!")
 
   expect_s3_class(df <- flytable_query("select FLYWIREsvid, hemibrain_match FROM fafb_hemilineages_survey WHERE hemibrain_match!=''", limit=10),
                   'data.frame')
