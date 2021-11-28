50b37d87ae20647b6f5be6cfc7aa42d6e71ef924
Gregory Jefferis <jefferis@gmail.com>
2021-11-28 21:08:30 +0000

    thinko in flywire_ids

--- R/flywire-urls.R
+++ R/flywire-urls.R
@@ -160,9 +160,9 @@ flywire_scene <- function(ids=NULL, open=FALSE) {
 flywire_ids <- function(x, ...) {
   if(is.data.frame(x)) {
     if("rootid" %in% colnames(x)) x=x[['rootid']]
-    else if("flywire.id" %in% colnames(x)) z=x[['flywire.id']]
-    else if("flywire_id" %in% colnames(x)) z=x[['flywire_id']]
-    else if("id" %in% colnames(x)) z=x[['id']]
+    else if("flywire.id" %in% colnames(x)) x=x[['flywire.id']]
+    else if("flywire_id" %in% colnames(x)) x=x[['flywire_id']]
+    else if("id" %in% colnames(x)) x=x[['id']]
     else {
       i64=sapply(x, bit64::is.integer64)
       if(sum(i64)==1) {
