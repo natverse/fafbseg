a16279ce8e9beb43f220b316e6a12e257cb9d429
Gregory Jefferis <jefferis@gmail.com>
2021-12-18 10:56:54 +0000

    update flytable tests for new db

--- tests/testthat/test-flytable.R
+++ tests/testthat/test-flytable.R
@@ -4,10 +4,10 @@ test_that("query works", {
   skip_if(inherits(ac, 'try-error'),
           "skipping flytable tests as unable to login!")
 
-  expect_s3_class(df <- flytable_query("select flywire_svid, hemibrain_match FROM fafb_hemilineages_survey WHERE hemibrain_match!=''", limit=10),
+  expect_s3_class(df <- flytable_query("select fruit_name, person FROM testfruit WHERE nid<3", limit=2L),
                   'data.frame')
-  expect_equal(nrow(df), 10L)
-  expect_s3_class(expect_warning(fruit <- flytable_list_rows('testfruit')), 'data.frame')
+  expect_equal(nrow(df), 2L)
+  expect_s3_class(fruit <- flytable_list_rows('testfruit'), 'data.frame')
   expect_equal(flytable_nrow('testfruit'), nrow(fruit))
   expect_true(
     flytable_update_rows(table = 'testfruit',
@@ -19,10 +19,11 @@ test_that("query works", {
 
   # now delete that row
   expect_true(nrow(iddf <- flytable_query("SELECT '_id' FROM testfruit WHERE person='Frederick the Great'"))>0)
+
   if(nrow(iddf)>10) {
     Sys.sleep(3)
     tfbase=flytable_base('testfruit')
-    pyiddf=reticulate::r_to_py(iddf)
-    tfbase$batch_delete_rows(table_name = 'testfruit', row_ids = pyiddf$values$tolist())
+    row_ids=reticulate::r_to_py(as.list(iddf[['_id']]))
+    tfbase$batch_delete_rows(table_name = 'testfruit', row_ids = row_ids)
   }
 })
