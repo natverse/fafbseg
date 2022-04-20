6bcf8a59e4edc41f0f89304092807cf84e2f7c31
Gregory Jefferis <jefferis@gmail.com>
2022-04-20 16:41:15 +0100

    fix urlencode
    * utils::URLencode has significant differences from curl::escape.
    * most important there are numerous characters that it won't encode eg /
    * curl::escape on a bare URL will mangle it
    * so need to parse url and then just escape the fragment

--- R/utils-urls.R
+++ R/utils-urls.R
@@ -3,6 +3,19 @@ urldecode <- function(x) {
   curl::curl_unescape(x)
 }
 
+# unfortunately curl::curl_escape escapes slashes in URLs!
+# and urltools::url_encode munges the #! that neuroglancer needs
+# if you give it a whole URL
 urlencode <- function(x) {
-  curl::curl_escape(x)
+  pu=httr::parse_url(x)
+  if(is.null(pu$scheme))
+    return(curl::curl_escape(x))
+  puf=pu$fragment
+  if(!is.null(puf) && substr(puf, 1,1)=="!") {
+    pu$fragment=paste0("!", curl::curl_escape(substr(puf, 2, nchar(puf))))
+  } else {
+    pu$fragment=curl::curl_escape(puf)
+  }
+  httr::build_url(pu)
 }
+
