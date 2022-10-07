4c82613bbd4281aecff2f8a81289fa77177c079e
Gregory Jefferis <jefferis@gmail.com>
2022-10-07 22:22:33 +0100

    add tests

---
+++ tests/testthat/test-ng-scene.R
@@ -0,0 +1,32 @@
+test_that("multiplication works", {
+
+  mirroreg = structure(
+    c(
+      -0.993507657550687,
+      -0.0789118732716515,
+      0.162578427057817,
+      0,
+      -0.0415891298774246,
+      0.998353717776582,
+      0.00339175766683097,
+      0,
+      0.0594215260756958,
+      0.0023521675167209,
+      0.9951539398219,
+      0,
+      1054823.85533788,
+      41754.6057995975,
+      -86025.0536428828,
+      1
+    ),
+    .Dim = c(4L,
+             4L)
+  )
+
+  expect_is(
+    sc2 <- flywire_scene() %>%
+    ngl_decode_scene %>%
+    xform(mirroreg, layers=c("mirror"="Production-segmentation_with_graph")),
+    "ngscene")
+  expect_true("mirror" %in% names(sc2$layers))
+})
