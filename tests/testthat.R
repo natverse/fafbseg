c955b9595d624b821acdd0d6c1048cb378b0e425
Gregory Jefferis <jefferis@gmail.com>
2025-04-07 12:04:34 +0100

    test: remove travis specifics
    * it's been a long time since we used travis

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,16 +1,6 @@
 library(testthat)
 library(fafbseg)
 
-# try to set token from environment variable on travis
-if(identical(Sys.getenv("TRAVIS"), "true")) {
-  token <- Sys.getenv('CHUNKEDGRAPH_SECRET')
-  if(nzchar(token)) {
-    message("Found token of length ", nchar(token), " in CHUNKEDGRAPH_SECRET")
-    p <- flywire_set_token(token)
-    message("written to: ", p)
-  }
-}
-
 if(nzchar(Sys.getenv("FLYWIRE_PRINCIPLES")))
   download_flywire_release_data()
 
