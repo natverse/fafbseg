ae8fd066cb31524092d747de71187d36741647ad
Gregory Jefferis <jefferis@gmail.com>
2025-04-07 12:26:32 +0100

    github: Give a dr_fafbseg status update on CI
    * trying to debug why python packages aren't being found
    * probably some python path issue on actions

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,6 +4,9 @@ library(fafbseg)
 if(nzchar(Sys.getenv("FLYWIRE_PRINCIPLES")))
   download_flywire_release_data()
 
+if(nzchar(Sys.getenv('CI')))
+  dr_fafbseg()
+
 op <- options('fafbseg.cachedir'=tempfile('fafbseg-tempcache'))
 test_check("fafbseg")
 options(op)
