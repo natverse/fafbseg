77da3a9d4f175f233e8f4a946dee8fed24657f12
Gregory Jefferis <jefferis@gmail.com>
2026-05-09 15:11:12 +0100

    Add FlyWire l2cache wrappers

--- R/l2cache.R
+++ R/l2cache.R
@@ -1,34 +1,230 @@
-flywire_l2data <- function(rootid=NULL, l2ids=NULL, attributes=NULL, rval=c('data.frame', "list")) {
-  if(!is.null(attributes) && is.character(attributes))
-    attributes=as.list(attributes)
-  rval=match.arg(rval)
-  fcc=flywire_cave_client()
-  if(is.null(l2ids) && is.null(rootid))
-    stop("You must provide one of l2ids or rootid!")
-  if(is.null(l2ids)){
-    stopifnot(length(rootid)==1L)
-    l2ids=flywire_l2ids(rootid)
+#' Query FlyWire level 2 cache attributes
+#'
+#' @param rootid One or more FlyWire root ids defining a segment. Supply either
+#'   \code{rootid} or \code{l2ids}.
+#' @param l2ids Optional vector of level 2 ids to query directly. Supply either
+#'   \code{rootid} or \code{l2ids}.
+#' @param attributes Optional character vector of attribute names to retrieve.
+#'   \code{NULL} requests all available attributes.
+#' @param split_columns Whether to expand multivalued attributes into separate
+#'   columns using the same names as python \code{caveclient}.
+#' @param rval Return either a \code{data.frame} or the raw named \code{list}
+#'   returned by python \code{caveclient}.
+#' @param ... Additional arguments passed to \code{\link{flywire_l2ids}} and to
+#'   recursive calls when \code{rootid} contains multiple ids.
+#' @inheritParams flywire_cave_client
+#'
+#' @return When \code{rval="data.frame"}, a tibble with one row per queried
+#'   level 2 id and an explicit \code{l2_id} column stored as a 64 bit integer.
+#'   When \code{rval="list"}, the raw named list returned by
+#'   \code{caveclient$l2cache$get_l2data()}. When \code{rootid} has length
+#'   greater than 1, a named list is returned with one result per root id.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' x <- flywire_l2attributes(
+#'   rootid = "720575940604351334",
+#'   attributes = c("size_nm3", "rep_coord_nm")
+#' )
+#' head(x)
+#' }
+flywire_l2attributes <- function(
+    rootid = NULL,
+    l2ids = NULL,
+    attributes = NULL,
+    split_columns = TRUE,
+    rval = c("data.frame", "list"),
+    datastack_name = getOption("fafbseg.cave.datastack_name", "flywire_fafb_production"),
+    ...) {
+  rval = match.arg(rval)
+  if(!is.null(attributes) && !is.character(attributes))
+    stop("attributes must be NULL or a character vector")
+  if(is.null(rootid) == is.null(l2ids))
+    stop("Please supply exactly one of rootid or l2ids")
+
+  if(!is.null(rootid)) {
+    rootid = flywire_ids(rootid, integer64 = FALSE)
+    if(length(rootid) > 1L) {
+      res = pbapply::pblapply(
+        rootid,
+        flywire_l2attributes,
+        attributes = attributes,
+        split_columns = split_columns,
+        rval = rval,
+        datastack_name = datastack_name,
+        ...
+      )
+      names(res) = rootid
+      return(res)
+    }
+    l2ids = flywire_l2ids(rootid, integer64 = TRUE, ...)
   }
-  pyids=rids2pyint(l2ids)
-  res=reticulate::py_call(fcc$l2cache$get_l2data,l2_ids = pyids, attributes=attributes)
-  resr=reticulate::py_to_r(res)
-  if(rval!='list')
-    list2df(resr, lists = 'list')
-  else resr
+
+  fcc = flywire_cave_client(datastack_name = datastack_name)
+  pyids = rids2pyint(l2ids)
+  pyattrs = if(is.null(attributes)) NULL else as.list(attributes)
+
+  if(rval == "list") {
+    res = reticulate::py_call(
+      fcc$l2cache$get_l2data,
+      l2_ids = pyids,
+      attributes = pyattrs
+    )
+    return(reticulate::py_to_r(res))
+  }
+
+  if(reticulate::py_has_attr(fcc$l2cache, "get_l2data_table")) {
+    res = reticulate::py_call(
+      fcc$l2cache$get_l2data_table,
+      l2_ids = pyids,
+      attributes = pyattrs,
+      split_columns = split_columns
+    )
+    return(pandas2df(res, keep_index = TRUE, tibble = TRUE))
+  }
+
+  res = reticulate::py_call(
+    fcc$l2cache$get_l2data,
+    l2_ids = pyids,
+    attributes = pyattrs
+  )
+  l2cache_data_to_tibble(reticulate::py_to_r(res), split_columns = split_columns)
 }
 
-flywire_l2volume <- function(rootids, cache=TRUE) {
-  rootids=flywire_ids(rootids, integer64 = FALSE)
-  fcc = flywire_cave_client()
-  if(length(rootids)>1)
-    return(pbapply::pbsapply(rootids, flywire_l2volume))
+#' Return FlyWire level 2 cache metadata
+#'
+#' @inheritParams flywire_cave_client
+#'
+#' @return A named list mapping available level 2 cache attributes to their
+#'   declared data types.
+#' @export
+flywire_l2cache_metadata <- function(
+    datastack_name = getOption("fafbseg.cave.datastack_name", "flywire_fafb_production")) {
+  fcc = flywire_cave_client(datastack_name = datastack_name)
+  fcc$l2cache$cache_metadata()
+}
 
-  fl2c=flywire_leaves_cache(subdir = file.path('flywire_l2volume', fcc$datastack_name))
-  vol=fl2c$get(rootids)
+#' Check whether an L2 cache is available
+#'
+#' @inheritParams flywire_cave_client
+#'
+#' @return Logical scalar indicating whether the current datastack has an
+#'   associated level 2 cache.
+#' @export
+flywire_has_l2cache <- function(
+    datastack_name = getOption("fafbseg.cave.datastack_name", "flywire_fafb_production")) {
+  fcc = flywire_cave_client(datastack_name = datastack_name)
+  fcc$l2cache$has_cache(datastack_name = datastack_name)
+}
+
+# This wraps a generic disk cache helper despite the historical function name.
+flywire_l2volume <- function(
+    rootids,
+    cache = TRUE,
+    datastack_name = getOption("fafbseg.cave.datastack_name", "flywire_fafb_production"),
+    ...) {
+  rootids = flywire_ids(rootids, integer64 = FALSE)
+  fcc = flywire_cave_client(datastack_name = datastack_name)
+  if(length(rootids) > 1L) {
+    return(pbapply::pbsapply(
+      rootids,
+      flywire_l2volume,
+      cache = cache,
+      datastack_name = datastack_name,
+      ...
+    ))
+  }
+
+  vol_cache = flywire_leaves_cache(
+    subdir = file.path("flywire_l2volume", fcc$datastack_name)
+  )
+  vol = if(cache) vol_cache$get(rootids) else cachem::key_missing()
   if(cachem::is.key_missing(vol)) {
-    vols=flywire_l2data(rootids, attributes='size_nm3')
-    vol=sum(vols[,1])
-    fl2c$set(rootids, vol)
+    vols = flywire_l2attributes(
+      rootid = rootids,
+      attributes = "size_nm3",
+      datastack_name = datastack_name,
+      ...
+    )
+    vol = sum(vols$size_nm3, na.rm = TRUE)
+    if(cache)
+      vol_cache$set(rootids, vol)
   }
   vol
+}
+
+l2cache_data_to_tibble <- function(x, split_columns = TRUE) {
+  if(!length(x))
+    return(tibble::tibble(l2_id = bit64::as.integer64(character())))
+
+  df = list2df(unname(x), lists = "list")
+  df$l2_id = bit64::as.integer64(names(x))
+  df = tibble::as_tibble(df[c("l2_id", setdiff(names(df), "l2_id"))])
+
+  if(!split_columns)
+    return(df)
+
+  df = l2cache_expand_column(
+    df,
+    "rep_coord_nm",
+    c("rep_coord_nm_x", "rep_coord_nm_y", "rep_coord_nm_z")
+  )
+  df = l2cache_expand_column(
+    df,
+    "pca",
+    c(
+      "pca_0_x", "pca_0_y", "pca_0_z",
+      "pca_1_x", "pca_1_y", "pca_1_z",
+      "pca_2_x", "pca_2_y", "pca_2_z"
+    )
+  )
+  df = l2cache_expand_column(
+    df,
+    "pca_val",
+    c("pca_val_0", "pca_val_1", "pca_val_2")
+  )
+  l2cache_expand_column(
+    df,
+    "chunk_intersect_count",
+    c(
+      "chunk_intersect_count_x_bottom",
+      "chunk_intersect_count_y_bottom",
+      "chunk_intersect_count_z_bottom",
+      "chunk_intersect_count_x_top",
+      "chunk_intersect_count_y_top",
+      "chunk_intersect_count_z_top"
+    )
+  )
+}
+
+l2cache_expand_column <- function(df, column, new_columns) {
+  if(!column %in% names(df))
+    return(df)
+
+  vals = df[[column]]
+  expanded = t(vapply(
+    vals,
+    l2cache_flatten_value,
+    FUN.VALUE = numeric(length(new_columns)),
+    n = length(new_columns)
+  ))
+  expanded = as.data.frame(expanded, stringsAsFactors = FALSE)
+  names(expanded) = new_columns
+  expanded = tibble::as_tibble(expanded)
+  df[[column]] = NULL
+  tibble::as_tibble(cbind(df, expanded))
+}
+
+l2cache_flatten_value <- function(x, n) {
+  if(is.null(x) || (length(x) == 1L && isTRUE(is.na(x))))
+    return(rep(NA_real_, n))
+
+  if(is.matrix(x) || length(dim(x)) == 2L)
+    x = as.numeric(t(x))
+  else
+    x = as.numeric(x)
+
+  length(x) = n
+  x
 }
---
+++ tests/testthat/test-l2cache.R
@@ -0,0 +1,63 @@
+test_that("l2cache_data_to_tibble preserves ids and expands columns", {
+  x = list(
+    "720575940600000001" = list(
+      size_nm3 = 10,
+      rep_coord_nm = c(1, 2, 3),
+      pca = matrix(1:9, nrow = 3, byrow = TRUE),
+      pca_val = c(4, 5, 6),
+      chunk_intersect_count = matrix(11:16, nrow = 2, byrow = TRUE)
+    ),
+    "720575940600000002" = list(
+      size_nm3 = 20,
+      rep_coord_nm = NA,
+      pca = NA,
+      pca_val = NA,
+      chunk_intersect_count = NA
+    )
+  )
+
+  res = fafbseg:::l2cache_data_to_tibble(x, split_columns = TRUE)
+
+  expect_equal(
+    as.character(res$l2_id),
+    c("720575940600000001", "720575940600000002")
+  )
+  expect_equal(res$size_nm3, c(10, 20))
+  expect_equal(res$rep_coord_nm_x, c(1, NA))
+  expect_equal(res$rep_coord_nm_y, c(2, NA))
+  expect_equal(res$rep_coord_nm_z, c(3, NA))
+  expect_equal(
+    unname(as.numeric(res[1, c(
+      "pca_0_x", "pca_0_y", "pca_0_z",
+      "pca_1_x", "pca_1_y", "pca_1_z",
+      "pca_2_x", "pca_2_y", "pca_2_z"
+    )])),
+    1:9
+  )
+  expect_equal(
+    unname(as.numeric(res[1, c(
+      "chunk_intersect_count_x_bottom",
+      "chunk_intersect_count_y_bottom",
+      "chunk_intersect_count_z_bottom",
+      "chunk_intersect_count_x_top",
+      "chunk_intersect_count_y_top",
+      "chunk_intersect_count_z_top"
+    )])),
+    11:16
+  )
+})
+
+test_that("l2cache_data_to_tibble can retain list columns", {
+  x = list(
+    "720575940600000003" = list(
+      size_nm3 = 30,
+      rep_coord_nm = c(7, 8, 9)
+    )
+  )
+
+  res = fafbseg:::l2cache_data_to_tibble(x, split_columns = FALSE)
+
+  expect_equal(as.character(res$l2_id), "720575940600000003")
+  expect_equal(res$size_nm3, 30)
+  expect_equal(res$rep_coord_nm[[1]], c(7, 8, 9))
+})
